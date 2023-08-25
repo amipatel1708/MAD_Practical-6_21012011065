8de50227188de49e05b27c278671bb3a72234811
asus <amipatidar383@gmail.com>
2023-08-25 23:18:01 +0530

    Practical 6

---
+++ app/src/main/java/com/amipatel/mad_practical_6_21012011065/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amipatel.mad_practical_6_21012011065
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
